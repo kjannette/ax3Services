af1c7c60cde26ea5bc495d60524e1f6f316c3670
Kenneth Jannette <sj@sjdev.co>
2024-01-22 10:03:38 -0600

    more

--- storageService/storeEditedCompletion.js
+++ storageService/storeEditedCompletion.js
@@ -14,13 +14,50 @@ function storeEditedCompletions(editedComps) {
   console.log(
     "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~storeEditedCompletion"
   );
-  const docId = editedComps.id;
+  let docId = editedComps.id;
   console.log("docId", docId);
   const dir = `EditedCompletions/${docId}/`;
 
   try {
     if (fs.existsSync(`EditedCompletions/${docId}/`)) {
-      console.log("The path exists.");
+      let temp = docId;
+      let bim;
+      const fileData = fs.readdirSync(`EditedCompletions/${docId}/`, "utf8");
+      console.log("fileData", fileData);
+      let foo = fileData.sort();
+      let bar = foo[foo.length - 1];
+      console.log("bar.split('-')[5]", bar.split("-")[5]);
+      const nameArray = bar.split("-");
+      const baz = bar.split("-")[5];
+      if (baz === "jbk") {
+        bim = `1`;
+        nameArray.splice(5, 0, bim);
+        const newone = nameArray.join("", "-");
+        console.log("~~~~~newone", newone);
+        const delimiter = "-";
+        const frak = nameArray.reduce((acc, val) =>
+          [].concat(acc, delimiter, val)
+        );
+        console.log("frak", frak.join(""));
+        const finished = frak.join("");
+        var options = { flag: "w" };
+        fs.writeFileSync(
+          dir + `${finished}`,
+          editedCompletes,
+          options,
+          function (err) {
+            if (err) {
+              return console.log("Error writing in storeEditedCompletion", err);
+            }
+          }
+        );
+        return;
+      } else {
+        bim = `-1-`;
+        nameArray.splice(4, 0, bim);
+        nameArray.join();
+        console.log("nameArray", nameArray);
+      }
     } else {
       makeDir(dir);
     }
