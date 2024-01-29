d269502d67a400c411f88f3392fb044d39f0fa49
Kenneth Jannette <sj@sjdev.co>
2024-01-29 12:22:25 -0600

    mor

--- paymentService/stripeController.js
+++ paymentService/stripeController.js
@@ -23,7 +23,7 @@ class StripeController {
       additionalAccounts,
       isAnnual
     );
-
+    const tokenId = token.id;
     let priceId;
     let addId;
     let items;
@@ -45,30 +45,92 @@ class StripeController {
       priceId = "price_1OdGRrBi8p7FeGFr2Zvr7USe";
     }
 
-    const tokenId = token.id;
+    items = [{ price: priceId }];
 
     if (planType === "partner" && isAnnual === false) {
       if (additionalAccounts === 1) {
         addId = "price_1OdIcqBi8p7FeGFrcKhFGudX";
+        items.push({ price: addId });
       } else if (additionalAccounts === 2) {
         addId = "price_1OdIvYBi8p7FeGFrLhvk1mwo";
+        items.push({ price: addId });
       }
     } else if (planType === "partner" && isAnnual === true) {
       if (additionalAccounts === 1) {
         addId = "price_1OdIunBi8p7FeGFrOJjULnA0";
+        items.push({ price: addId });
       } else if (additionalAccounts === 2) {
         addId = "price_1OdJ0pBi8p7FeGFrFrrXjfwA";
+        items.push({ price: addId });
       }
     }
 
-    if (additionalAccounts === 0) {
-      items = [{ price: priceId }];
-    } else if (additionalAccounts === 1) {
-      items = [{ price: priceId }, { price: addId }];
-    } else if (additionalAccounts === 2) {
-      items = [{ price: priceId }, { price: addId }];
+    if (planType === "seniorPartner" && isAnnual === false) {
+      if (additionalAccounts === 1) {
+        addId = "price_1OdJ1XBi8p7FeGFrZVHDlSGH";
+        items.push({ price: addId });
+      } else if (additionalAccounts === 2) {
+        addId = "price_1OdJ3vBi8p7FeGFr47ZbQQN6";
+        items.push({ price: addId });
+      } else if (additionalAccounts === 3) {
+        addId = "price_1OdJ50Bi8p7FeGFrFB8wTpZu";
+        items.push({ price: addId });
+      } else if (additionalAccounts === 4) {
+        addId = "price_1OdJ6uBi8p7FeGFrdUhzYaGC";
+        items.push({ price: addId });
+      } else if (additionalAccounts === 5) {
+        addId = "price_1OdJ88Bi8p7FeGFrSwpR5r4Z";
+        items.push({ price: addId });
+      } else if (additionalAccounts === 6) {
+        addId = "price_1OdJ9CBi8p7FeGFrTQNy2YVd";
+        items.push({ price: addId });
+      } else if (additionalAccounts === 7) {
+        addId = "price_1OdJAIBi8p7FeGFrZyWHIlYf";
+        items.push({ price: addId });
+      } else if (additionalAccounts === 8) {
+        addId = "price_1OdJCzBi8p7FeGFriBy3OCso";
+        items.push({ price: addId });
+      } else if (additionalAccounts === 9) {
+        addId = "price_1OddLzBi8p7FeGFrzeVmj4fG";
+        items.push({ price: addId });
+      } else if (additionalAccounts === 10) {
+        addId = "price_1OddNUBi8p7FeGFraRX3ypOK";
+        items.push({ price: addId });
+      }
+    } else if (planType === "seniorPartner" && isAnnual === true) {
+      if (additionalAccounts === 1) {
+        addId = "price_1OdJ3EBi8p7FeGFr6371XcAi";
+        items.push({ price: addId });
+      } else if (additionalAccounts === 2) {
+        addId = "price_1OdJ4PBi8p7FeGFrgW7aKHwZ";
+        items.push({ price: addId });
+      } else if (additionalAccounts === 3) {
+        addId = "price_1OdJ6IBi8p7FeGFrItKkXCeM";
+        items.push({ price: addId });
+      } else if (additionalAccounts === 4) {
+        addId = "price_1OdJ7YBi8p7FeGFrCC6AulEY";
+        items.push({ price: addId });
+      } else if (additionalAccounts === 5) {
+        addId = "price_1OdJ8lBi8p7FeGFr6d8z3afK";
+        items.push({ price: addId });
+      } else if (additionalAccounts === 6) {
+        addId = "price_1OdJ9kBi8p7FeGFrxXTG7s20";
+        items.push({ price: addId });
+      } else if (additionalAccounts === 7) {
+        addId = "price_1OdJBJBi8p7FeGFrc0F6ByO5";
+        items.push({ price: addId });
+      } else if (additionalAccounts === 8) {
+        addId = "price_1OdJDbBi8p7FeGFrbpE4URLd";
+        items.push({ price: addId });
+      } else if (additionalAccounts === 9) {
+        addId = "price_1OddMxBi8p7FeGFraD12KrD4";
+        items.push({ price: addId });
+      } else if (additionalAccounts === 10) {
+        addId = "price_1OddOEBi8p7FeGFrKcFey7PM";
+        items.push({ price: addId });
+      }
     }
-    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>>>items", items);
+
     try {
       // create new customer object
       const customer = await stripe.customers.create({
