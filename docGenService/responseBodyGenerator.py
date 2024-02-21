e5652083702fc0acfcd77dc8a42f8312fb813d66
Kenneth Jannette <sj@sjdev.co>
2024-02-20 19:21:09 -0600

    more

--- docGenService/responseBodyGenerator.py
+++ docGenService/responseBodyGenerator.py
@@ -28,19 +28,11 @@ class GenerateBody(object):
         firmState = None
         firmZip = None
         firmTel = None
+        reqFile = None
+        respFile = None
         # filePath = f"/Users/kjannette/workspace/ax3/ax3Services/docGenService/Docxstaging/{docId}.docx"
         # f = open(filePath, "rb")
         document = Document()
-        print("~~~~~~~~~~~+++++++++++docId, Firmsatte in responseBodyGenerator", docId)
-        # reqFile = f"/Users/kjannette/workspace/ax3/ax3Services/Documents/Requests/Parsedrogs/{docId}/{docId}-jbk-parsedRequests.json"
-        # respFile = f"/Users/kjannette/workspace/ax3/ax3Services/Documents/Responses/Rogresp/{docId}/{docId}-jbk-responses.json"
-
-        reqFile = f"/var/www/ax3Services/Documents/Requests/Parsedrogs/{docId}/{docId}-jbk-parsedRequests.json"
-        respFile = f"/var/www/ax3Services/Documents/Responses/Rogresp/{docId}/{docId}-jbk-responses.json"
-
-        # reqFile = f"/Users/kjannette/workspace/ax3/ax3Services/Documents/Requests/Parsedrogs/{docId}/{docId}-jbk-parsedRequests.json"
-        # respFile = f"/Users/kjannette/workspace/ax3/ax3Services/Documents/Responses/Rogresp/{docId}/{docId}-jbk-responses.json"
-
         dataFile = f"Docxinfo/{docId}.json"
 
         # Get case data
@@ -66,10 +58,20 @@ class GenerateBody(object):
         firmState = jsonData.get("state")
         firmTel = jsonData.get("tel")
         firmZip = jsonData.get("firmZip")
-        print(
-            "````````````````````````````````````````````````````````firmState",
-            firmState,
-        )
+
+        if reqType == "interrogatories":
+            reqFile = f"/var/www/ax3Services/Documents/Requests/Parsedrogs/{docId}/{docId}-jbk-parsedRequests.json"
+            respFile = f"/var/www/ax3Services/Documents/Responses/Rogresp/{docId}/{docId}-jbk-responses.json"
+        elif reqType == "combined-numbered":
+            reqFile = f"/var/www/ax3Services/Documents/Requests/Parsedcombined/{docId}/{docId}-jbk-parsedRequests.json"
+            respFile = f"/var/www/ax3Services/Documents/Responses/Combinedresp/{docId}/{docId}-jbk-responses.json"
+        elif reqType == "production":
+            reqFile = f"/var/www/ax3Services/Documents/Requests/Parsedprod/{docId}/{docId}-jbk-parsedRequests.json"
+            respFile = f"/var/www/ax3Services/Documents/Responses/Prodresp/{docId}/{docId}-jbk-responses.json"
+        elif reqType == "admissions":
+            reqFile = f"/var/www/ax3Services/Documents/Requests/Parsedadmit/{docId}/{docId}-jbk-parsedRequests.json"
+            respFile = f"/var/www/ax3Services/Documents/Responses/Admitresp/{docId}/{docId}-jbk-responses.json"
+
         if clientPosition == "Plaintiff":
             respondent = plaintiff
         elif clientPosition == "Defendant":
@@ -227,7 +229,6 @@ class GenerateBody(object):
         )
         paragraph.paragraph_format.space_before = Pt(24)
         paragraph.paragraph_format.space_after = Pt(12)
-        # document.save(f"/var/www/ax3Services/Docxfinal/{docId}.docx")
         document.save(f"/var/www/ax3Services/Docxfinal/{docId}.docx")
 
 
@@ -237,3 +238,8 @@ class GenerateBody(object):
 # Uncomment for development/smoke testing
 # genBod = GenerateBody()
 # genBod.generate("a93d9b76-28aa-4b8c-83e4-e87ec6294c4b")
+# reqFile = f"/Users/kjannette/workspace/ax3/ax3Services/Documents/Requests/Parsedrogs/{docId}/{docId}-jbk-parsedRequests.json"
+# respFile = f"/Users/kjannette/workspace/ax3/ax3Services/Documents/Responses/Rogresp/{docId}/{docId}-jbk-responses.json"
+
+# reqFile = f"/Users/kjannette/workspace/ax3/ax3Services/Documents/Requests/Parsedrogs/{docId}/{docId}-jbk-parsedRequests.json"
+# respFile = f"/Users/kjannette/workspace/ax3/ax3Services/Documents/Responses/Rogresp/{docId}/{docId}-jbk-responses.json"
