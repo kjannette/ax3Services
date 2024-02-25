415efe88de6c1d10c98bd1eef74d888a0ab47266
Kenneth Jannette <sj@sjdev.co>
2024-02-25 15:32:00 -0600

    more

--- docConvertService/pyserv.py
+++ docConvertService/pyserv.py
@@ -3,7 +3,7 @@ from bottle import Bottle, run, get, post, request
 app = Bottle()
 
 @app.route('/newdoc/<id:int>')
-def hello():
+def newdoc():
     print('id:', id)
 
 run(app, host='localhost', port=5050)
