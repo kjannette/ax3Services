b7cdfc2153c93a2f58e6d4737e1373429f5cb8f3
Kenneth Jannette <sj@sjdev.co>
2024-03-02 16:18:26 -0600

    more

---
+++ DocumentConstants/foundationRogs.js
@@ -0,0 +1,10 @@
+export const forCorporation = [
+  {
+    interrogatoryId: "c42293a4-77fd-4c1f-926d-72c36af01298",
+    text: "Identify the indivudal responding to these reusts.  Set forth the indivdual's name, address, telephone number.",
+  },
+  {
+    interrogatoryId: "17822ae2-d8ae-4d76-aff6-2c0740d7ba0e",
+    text: "For the person verifying these answers, state the date of first employment with you, and the datesand titles of each job position the person has held while employed by you.",
+  },
+];
