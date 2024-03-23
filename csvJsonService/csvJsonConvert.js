583b9969c575c07f44bbfdb44165b0658b190b41
Kenneth Jannette <sj@sjdev.co>
2024-03-23 10:07:11 -0500

    more

---
+++ csvJsonService/csvJsonConvert.js
@@ -0,0 +1,27 @@
+//https://www.npmjs.com/package/csvtojson
+//https://www.npmjs.com/package/json-2-csv
+
+const request = require("request");
+const csv = require("csvtojson");
+
+function onError(err) {
+  console.log(err);
+}
+
+function onComplete() {
+  console.log("done");
+}
+
+csv()
+  .fromStream(request.get("http://mywebsite.com/mycsvfile.csv"))
+  .subscribe(
+    (json) => {
+      return new Promise((resolve, reject) => {
+        // long operation for each json e.g. transform / write into database.
+      }).then((res) => {
+        console.log(res);
+      });
+    },
+    onError,
+    onComplete
+  );
