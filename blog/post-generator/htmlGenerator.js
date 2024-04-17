e0627a84629b079896f67e65fda7edbe4eac4aff
Kenneth Jannette <sj@sjdev.co>
2024-04-16 19:26:54 -0500

    More

--- blog/post-generator/htmlGenerator.js
+++ blog/post-generator/htmlGenerator.js
@@ -1,26 +1,35 @@
 const generateHead = (title, scripts) => {
-  const scriptTags = scripts.map(src => `<script async src="${src}"></script>`).join('');
+  const scriptTags = scripts
+    .map((src) => `<script async src="${src}"></script>`)
+    .join("");
   return `
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="./style.css" />
-  <link rel="icon" href="./favicon.ico" type="image/x-icon" />
-  <title>${title}</title>
-  ${scriptTags}
-</head>
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="stylesheet" href="./style.css" />
+      <link rel="icon" href="./favicon.ico" type="image/x-icon" />
+      <title>${title}</title>
+      ${scriptTags}
+    </head>
 `;
 };
 
 const generateBody = (subject, content) => {
-  const paragraphs = content.split('\n').map(p => `<p class="body-text">${p}</p>`).join('');
+  const paragraphs = content
+    .split("\n")
+    .map((p) => `<p class="body-text">${p}</p>`)
+    .join("");
   return `
-<body>
-  <div class="main-content-container">
-    <h1>${subject}</h1>
-    ${paragraphs}
-  </div>
-</body>
+  <body>
+    <div class="main-content-container">
+      <div class="heading-container">
+        <h1>${subject}</h1>
+      </div>
+      <div class="body-text-container">
+        ${paragraphs}
+      </div>
+    </div>
+  </body>
 `;
 };
 
